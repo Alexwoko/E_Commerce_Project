7012448b2518912bf0657050c2311ec3fa87c811
Alexwoko <36886799+Alexwoko@users.noreply.github.com>
2023-11-21 20:25:59 +0100

    Delete Database/TestScripts/MasterTestScript.sh

--- Database/TestScripts/MasterTestScript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# TestScript.sh
-# RUN IN BASH CONTEXT:
-# bash /Users/alextao/ECommerceProject/ECommerceProject/Database/TestScripts/MasterTestScript.sh
-# (bash -u root -p < /Users/alextao/ECommerceProject/ECommerceProject/Database/TestScripts/MasterTestScript.sh)
-
-
-# RUN ALL TEST SCRIPTS FROM THIS SCRIPT
